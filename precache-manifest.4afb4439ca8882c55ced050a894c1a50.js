8cec257e0a144ef96e73d895966e7c053b5da524
Manel2313 <djemal.manel@gmail.com>
2019-05-07 15:27:56 +0100

    Updates

---
+++ precache-manifest.4afb4439ca8882c55ced050a894c1a50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3489317dc64c94b7734a560175f5406",
+    "url": "/Credit-Card/index.html"
+  },
+  {
+    "revision": "47a5d8914a292dd6de8b",
+    "url": "/Credit-Card/static/css/main.7e7c6b8f.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/Credit-Card/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "47a5d8914a292dd6de8b",
+    "url": "/Credit-Card/static/js/main.38a21940.chunk.js"
+  },
+  {
+    "revision": "1425aa8bd00b6e03511e",
+    "url": "/Credit-Card/static/js/runtime~main.35f599d6.js"
+  },
+  {
+    "revision": "2c7243c765bf8fafda26fe878f7e2d1a",
+    "url": "/Credit-Card/static/media/background.2c7243c7.png"
+  },
+  {
+    "revision": "5ac573dff32d4a9852dbb7073948edf2",
+    "url": "/Credit-Card/static/media/master-card.5ac573df.png"
+  },
+  {
+    "revision": "8bc50203ffcbf69c7e5c754558d98325",
+    "url": "/Credit-Card/static/media/puce.8bc50203.png"
+  },
+  {
+    "revision": "8486189aadc4809ebd8edfe89452e223",
+    "url": "/Credit-Card/static/media/visa.8486189a.png"
+  }
+]);
